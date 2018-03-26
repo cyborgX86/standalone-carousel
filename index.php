f874755b4934463564ed74f9c6e335aa1f7a43e7
n7rc <notrec@hotmail.com>
2018-03-26 17:53:39 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-/* index.php - n7rc */
 
 /*
 Muestra información y noticias mediante un carrusel de imágenes. Para que una
@@ -11,7 +10,6 @@ de cola de reproducción y la imagen será eliminada.
 require_once ('config.inc.php');
 require_once ('functions.inc.php');
 
-/* Inicialización de variables */
 $path  = getPool($pool)[0]; // ubicación de las imágenes.
 $files = getPool($pool)[1]; // elementos del carrusel.
 ?>
@@ -21,26 +19,26 @@ $files = getPool($pool)[1]; // elementos del carrusel.
 <head>
     <meta charset="utf-8">
     <title><?php echo ($title); ?></title>
-		<meta http-equiv="refresh" content="<?php echo refreshTime($pool); ?>">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link href="css/bootstrap.min.css" rel="stylesheet">
-		<link href="css/style.css" rel="StyleSheet" type="text/css">
-		<script src="jquery/jquery-3.1.1.min.js"></script>
-		<script src="bootstrap/bootstrap.min.js"></script>
+    <meta http-equiv="refresh" content="<?php echo refreshTime($pool); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="StyleSheet" type="text/css">
+    <script src="jquery/jquery-3.1.1.min.js"></script>
+    <script src="bootstrap/bootstrap.min.js"></script>
     <script type="text/javascript">
     // Carrusel/indicadores.
     $(function(){
-		    $('#slider div:gt(0)').hide();
-		    setInterval(function(){
+	$('#carousel div:gt(0)').hide();
+	setInterval(function(){
           // control carrusel
-          $('#slider div:first-child').fadeOut(<?php echo ($fadeOut); ?>)
-          .next('div').fadeIn(<?php echo ($fadeIn); ?>).end().appendTo('#slider');
+          $('#carousel div:first-child').fadeOut(<?php echo ($fadeOut); ?>)
+          .next('div').fadeIn(<?php echo ($fadeIn); ?>).end().appendTo('#carousel');
           // control indicadores
           $('.indicators li.active').removeClass('active').next()
           .add('.indicators li:first').last().addClass('active');
         }, <?php echo ($timeFocus * 1000); ?>); // milisegundos.
-		});
-		</script>
+      });
+      </script>
 
 </head>
 <body>
@@ -48,7 +46,7 @@ $files = getPool($pool)[1]; // elementos del carrusel.
 <?php printHeader($header) ?>
 
 <div style="margin-top: 3%; margin-bottom: 1%;">
-	<div align="center" class="col-md-12 col-sm-12  col-xs-12" style="width: 100%; height: 100%;">
+  <div align="center" class="col-md-12 col-sm-12  col-xs-12" style="width: 100%; height: 100%;">
     <div id="carrousel">
         <?php getCarousel($pool); ?>
     </div>
