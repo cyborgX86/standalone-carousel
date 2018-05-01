af380b9dfdd49e80dcade3c135cdd6e7c83b649f
n7rc <notrec@hotmail.com>
2018-05-01 19:01:56 +0200

    Add files via upload

--- functions.inc.php
+++ functions.inc.php
@@ -2,24 +2,22 @@
 
 /*getPool() obtiene las imágenes y ubicación de la carpeta de reproducción del
 Slider.*/
-
 function getPool($path){
 
-    if(substr($path, -1) != "/") $path .= "/"; // añade "/" al final de la carpeta.
-    $dir = opendir($path);
-    $files = array();
+  if(substr($path, -1) != "/") $path .= "/"; // añade "/" al final de la carpeta.
+  $dir = opendir($path);
+  $files = array();
 
-    while ($current = readdir($dir)){
-        if( $current != "." && $current != "..") {
-            if(is_dir($path.$current)) {
-                getPool($path.$current);
-            }
-            else {
-                $files[] = $current;
-            }
-        }
+  while ($current = readdir($dir)){
+    if( $current != "." && $current != "..") {
+      if(is_dir($path.$current)) {
+        getPool($path.$current);
+      } else {
+        $files[] = $current;
+      }
     }
-return array($path, $files);
+  }
+  return array($path, $files);
 }
 
 /*getCarousel() obtiene la cola de reproducción incorporando las imagenes al Slider
@@ -28,43 +26,41 @@ actual saca la imagen de cola de reproducción. Si el nombre del archivo no comi
 por una fecha válida no entra en cola de reproducción.*/
 
 function getCarousel($path){
-    global $path;
-    global $files;
-    global $widthImage;
-    global $heightImage;
+  global $path;
+  global $files;
+  global $widthImage;
+  global $heightImage;
 
-    shuffle($files); // desordena el array de forma aleatoria.
+  shuffle($files); // desordena el array de forma aleatoria.
 
-    for($i=0; $i<count( $files ); $i++){
-      if (is_numeric(substr($files[$i],0,8)) & substr($files[$i],0,8) >= Date("Ymd")){
-        echo '<div style="width: ' . $widthImage . 'px; height: ' . $heightImage . 'px;
-              max-height: ' . $heightImage . 'px;">
-              <img style="width: ' . $widthImage . 'px; height: ' . $heightImage . 'px
-              " src="' . $path . $files[$i] . '" />
-              </div>';
-      }
+  for($i=0; $i<count( $files ); $i++){
+    if (is_numeric(substr($files[$i],0,8)) & substr($files[$i],0,8) >= Date("Ymd")){
+      echo '<div style="width: ' . $widthImage . 'px; height: ' . $heightImage . 'px;
+            max-height: ' . $heightImage . 'px;">
+            <img style="width: ' . $widthImage . 'px; height: ' . $heightImage . 'px
+            " src="' . $path . $files[$i] . '" />
+            </div>';
     }
+  }
 }
 
 /*getIndicators() obtiene el número de imágenes en la cola de reproducción generando
 un elemento <li> para cada uno de ellos dentro de la clase css "indicartors".*/
-
 function getIndicators($path){
   global $files;
 
-    //Se imprime un solo elemento de la clase "active".
-    echo '<li class="active"><em></em></li>';
+  //Se imprime un solo elemento de la clase "active".
+  echo '<li class="active"><em></em></li>';
 
-    //Se imprime la lista de elementos (<li> - 1).
-    for($i=0; $i<(count( $files ) - 1); $i++){
-      if (is_numeric(substr($files[$i],0,8)) & substr($files[$i],0,8) >= Date("Ymd")){
-        echo '<li><em></em></li>';
-      }
+  //Se imprime la lista de elementos (<li> - 1).
+  for($i=0; $i<(count( $files ) - 1); $i++){
+    if (is_numeric(substr($files[$i],0,8)) & substr($files[$i],0,8) >= Date("Ymd")){
+      echo '<li><em></em></li>';
     }
+  }
 }
 
 /*printHeader() imprime la barra de cabecera si $header está definido a True.*/
-
 function printHeader($header){
   global $header;
   global $titleHeader;
@@ -74,18 +70,18 @@ function printHeader($header){
 
   if ($header === True){
     echo '<nav style="margin: 0px; height: 3%; width: 100%;" class="navbar navbar-default">
-  	       <div style="margin: 0px; height: 3%;"class="container-fluid">
-  		     <div class="navbar-header">
-  			      <a class="navbar-brand">' . $titleHeader . '</a>
-  			      <a class="navbar-brand">:::: ' . $subTitleHeader . ' ::::</a>
-  		     </div>
-  		     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-  			      <ul class="nav navbar-nav">
-  				        <li class="active"><a>Canal: #' . $chanelName . '</a></li>
+            <div style="margin: 0px; height: 3%;"class="container-fluid">
+              <div class="navbar-header">
+                <a class="navbar-brand">' . $titleHeader . '</a>
+                <a class="navbar-brand">:::: ' . $subTitleHeader . ' ::::</a>
+              </div>
+              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                  <li class="active"><a>Canal: #' . $chanelName . '</a></li
                   <li class="active"><a>:::: ' . $subTitleChanel . ' ::::</a></li>
-  			      </ul>
-  		     </div>
-  	       </div>
+                </ul>
+              </div>
+  	        </div>
           </nav>';
   }else{
     return;
@@ -94,33 +90,31 @@ function printHeader($header){
 
 /*refreshTime() devuelva el tiempo de refresco en el navegador. El tiempo de refresco
 es múltiplo de las imágenes en cola de reproducción.*/
-
 function refreshTime($path){
-    global $files;
-    global $timeFocus;
-    $imagesIn = array();
+  global $files;
+  global $timeFocus;
+  $imagesIn = array();
 
-    for($i=0; $i<count( $files ); $i++){
-        if (substr($files[$i],0,8) >= Date("Ymd")){
-        array_push($imagesIn, $files[$i]);
-      }
+  for($i=0; $i<count( $files ); $i++){
+    if (substr($files[$i],0,8) >= Date("Ymd")){
+      array_push($imagesIn, $files[$i]);
     }
-$refreshTime = count($imagesIn) * $timeFocus;
-return ($refreshTime);
+  }
+  $refreshTime = count($imagesIn) * $timeFocus;
+  return ($refreshTime);
 }
 
 /*delFiles() elimina los ficheros que inician su nombre con fecha menor a la fecha
 del sistema.*/
-
 function delFiles($path){
   global $path;
   global $files;
 
-    for($i=0; $i<count( $files ); $i++){
-      if (is_numeric(substr($files[$i],0,8)) & substr($files[$i],0,8) < Date("Ymd")){
-        unlink ($path . $files[$i]);
-      }
+  for($i=0; $i<count( $files ); $i++){
+    if (is_numeric(substr($files[$i],0,8)) & substr($files[$i],0,8) < Date("Ymd")){
+      unlink ($path . $files[$i]);
     }
-return;
+  }
+  return;
 }
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+/*
+Pendientes:
+Adaptar tamaño a pantalla
+*/
 
 /*
 Muestra información y noticias mediante un carrusel de imágenes. Para que una
@@ -18,25 +22,25 @@ $files = getPool($pool)[1]; // Elementos del carrusel.
 <html lang="es">
 
 <head>
-    <meta charset="utf-8">
-    <title><?php echo ($title); ?></title>
-    <meta http-equiv="refresh" content="<?php echo (refreshTime($pool)); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="StyleSheet" type="text/css">
-    <script src="js/jquery-3.1.1.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script type="text/javascript">
+  <meta charset="utf-8">
+  <title><?php echo ($title); ?></title>
+  <meta http-equiv="refresh" content="<?php echo (refreshTime($pool)); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/style.css" rel="StyleSheet" type="text/css">
+  <script src="js/jquery-3.1.1.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script type="text/javascript">
     //Carrusel/Indicadores
     $(function(){
-        $('#carousel div:gt(0)').hide();
-        setInterval(function(){
-           // control carrusel
-          $('#carousel div:first-child').fadeOut(<?php echo ($fadeOut); ?>)
-          .next('div').fadeIn(<?php echo ($fadeIn); ?>).end().appendTo('#carousel');
-          // control indicadores
-          $('.indicators li.active').removeClass('active').next()
-          .add('.indicators li:first').last().addClass('active');
+      $('#carousel div:gt(0)').hide();
+      setInterval(function(){
+        // control carrusel
+        $('#carousel div:first-child').fadeOut(<?php echo ($fadeOut); ?>)
+        .next('div').fadeIn(<?php echo ($fadeIn); ?>).end().appendTo('#carousel');
+        // control indicadores
+        $('.indicators li.active').removeClass('active').next()
+        .add('.indicators li:first').last().addClass('active');
       }, <?php echo ($timeFocus * 1000); ?>); // milisegundos.
     });
   </script>
@@ -46,9 +50,9 @@ $files = getPool($pool)[1]; // Elementos del carrusel.
 <?php printHeader($header) ?>
 
 <div style="margin-top: 3%; margin-bottom: 1%;">
-	<div align="center" class="col-md-12 col-sm-12  col-xs-12" style="width: 100%; height: 100%;">
+  <div align="center" class="col-md-12 col-sm-12  col-xs-12" style="width: 100%; height: 100%;">
     <div id="carousel">
-        <?php getCarousel($pool); ?>
+      <?php getCarousel($pool); ?>
     </div>
   </div>
 </div>
